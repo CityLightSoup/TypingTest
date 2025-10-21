f8605b22f1d2fdca02b2c212c4fbd24917ceff24
CityLightSoup <guibonpia@gmail.com>
2025-10-21 10:23:19 +0900

    feat/#3: サウンドの変更

--- src/Components/Sound.jsx
+++ src/Components/Sound.jsx
@@ -2,26 +2,26 @@ import React, { forwardRef, useImperativeHandle } from "react";
 import useSound from 'use-sound'
 
 //サウンドのインポート
-import r1s0 from '../Sounds/keySound0.wav'
-import r1s1 from '../Sounds/keySound1.wav'
-import r1s2 from '../Sounds/keySound2.wav'
-import r1s3 from '../Sounds/keySound3.wav'
-import r1s4 from '../Sounds/keySound4.wav'
-import r1s5 from '../Sounds/keySound5.wav'
-import r1s6 from '../Sounds/keySound6.wav'
-import r1s7 from '../Sounds/keySound7.wav'
-import r1s8 from '../Sounds/keySound8.wav'
-import r1s9 from '../Sounds/keySound9.wav'
-import r2s0 from '../Sounds/keyS20.wav'
-import r2s1 from '../Sounds/keyS21.wav'
-import r2s2 from '../Sounds/keyS22.wav'
-import r2s3 from '../Sounds/keyS23.wav'
-import r2s4 from '../Sounds/keyS24.wav'
-import r2s5 from '../Sounds/keyS25.wav'
-import r2s6 from '../Sounds/keyS26.wav'
-import r2s7 from '../Sounds/keyS27.wav'
-import r2s8 from '../Sounds/keyS28.wav'
-import r2s9 from '../Sounds/keyS29.wav'
+import r1s0 from '../Sounds/s/keySound0.wav'
+import r1s1 from '../Sounds/s/keySound1.wav'
+import r1s2 from '../Sounds/s/keySound2.wav'
+import r1s3 from '../Sounds/s/keySound3.wav'
+import r1s4 from '../Sounds/s/keySound4.wav'
+import r1s5 from '../Sounds/s/keySound5.wav'
+import r1s6 from '../Sounds/s/keySound6.wav'
+import r1s7 from '../Sounds/s/keySound7.wav'
+import r1s8 from '../Sounds/s/keySound8.wav'
+import r1s9 from '../Sounds/s/keySound9.wav'
+import r2s0 from '../Sounds/s/keyS20.wav'
+import r2s1 from '../Sounds/s/keyS21.wav'
+import r2s2 from '../Sounds/s/keyS22.wav'
+import r2s3 from '../Sounds/s/keyS23.wav'
+import r2s4 from '../Sounds/s/keyS24.wav'
+import r2s5 from '../Sounds/s/keyS25.wav'
+import r2s6 from '../Sounds/s/keyS26.wav'
+import r2s7 from '../Sounds/s/keyS27.wav'
+import r2s8 from '../Sounds/s/keyS28.wav'
+import r2s9 from '../Sounds/s/keyS29.wav'
 // import r3s0 from '../Sounds/keyR3.mp3' //効果音ラボ　カーソル移動12
 // import r3s1 from '../Sounds/keyR3.mp3'
 // import r3s2 from '../Sounds/keyR3.mp3'
@@ -47,26 +47,26 @@ import beepSound from '../Sounds/beep.mp3'
 
 
 export const Sound = forwardRef(({ round } , ref) => {
-    const [play_r1s0] = useSound(r1s0);
-    const [play_r1s1] = useSound(r1s1);
-    const [play_r1s2] = useSound(r1s2);
-    const [play_r1s3] = useSound(r1s3);
-    const [play_r1s4] = useSound(r1s4);
-    const [play_r1s5] = useSound(r1s5);
-    const [play_r1s6] = useSound(r1s6);
-    const [play_r1s7] = useSound(r1s7);
-    const [play_r1s8] = useSound(r1s8);
-    const [play_r1s9] = useSound(r1s9);
-    const [play_r2s0] = useSound(r2s0, { volume: 0.2 });
-    const [play_r2s1] = useSound(r2s1, { volume: 0.2 });
-    const [play_r2s2] = useSound(r2s2, { volume: 0.2 });
-    const [play_r2s3] = useSound(r2s3, { volume: 0.2 });
-    const [play_r2s4] = useSound(r2s4, { volume: 0.2 });
-    const [play_r2s5] = useSound(r2s5, { volume: 0.2 });
-    const [play_r2s6] = useSound(r2s6, { volume: 0.2 });
-    const [play_r2s7] = useSound(r2s7, { volume: 0.2 });
-    const [play_r2s8] = useSound(r2s8, { volume: 0.2 });
-    const [play_r2s9] = useSound(r2s9, { volume: 0.2 });
+    const [play_r1s0] = useSound(r1s0, { volume: 0.4});
+    const [play_r1s1] = useSound(r1s1, { volume: 0.4});
+    const [play_r1s2] = useSound(r1s2, { volume: 0.4});
+    const [play_r1s3] = useSound(r1s3, { volume: 0.4});
+    const [play_r1s4] = useSound(r1s4, { volume: 0.4});
+    const [play_r1s5] = useSound(r1s5, { volume: 0.4});
+    const [play_r1s6] = useSound(r1s6, { volume: 0.4});
+    const [play_r1s7] = useSound(r1s7, { volume: 0.4});
+    const [play_r1s8] = useSound(r1s8, { volume: 0.4});
+    const [play_r1s9] = useSound(r1s9, { volume: 0.4});
+    const [play_r2s0] = useSound(r2s0, { volume: 3 });
+    const [play_r2s1] = useSound(r2s1, { volume: 3 });
+    const [play_r2s2] = useSound(r2s2, { volume: 3 });
+    const [play_r2s3] = useSound(r2s3, { volume: 3 });
+    const [play_r2s4] = useSound(r2s4, { volume: 3 });
+    const [play_r2s5] = useSound(r2s5, { volume: 3 });
+    const [play_r2s6] = useSound(r2s6, { volume: 3 });
+    const [play_r2s7] = useSound(r2s7, { volume: 3 });
+    const [play_r2s8] = useSound(r2s8, { volume: 3 });
+    const [play_r2s9] = useSound(r2s9, { volume: 3 });
     // const [play_r3s0] = useSound(r3s0, { volume: 0.2 });
     // const [play_r3s1] = useSound(r3s1, { volume: 0.2 });
     // const [play_r3s2] = useSound(r3s2, { volume: 0.2 });
@@ -87,8 +87,8 @@ export const Sound = forwardRef(({ round } , ref) => {
     // const [play_r4s7] = useSound(r4s7, { volume: 0.2 });
     // const [play_r4s8] = useSound(r4s8, { volume: 0.2 });
     // const [play_r4s9] = useSound(r4s9, { volume: 0.2 });
-    const [playPractice] = useSound(pras0, { volume: 0.2 });
-    const [playBeep] = useSound(beepSound, { volume: 0.2 });
+    const [playPractice] = useSound(pras0, { volume: 0.1 });
+    const [playBeep] = useSound(beepSound, { volume: 0.05 });
 
     const soundPlayersPerRound = {
         1: [play_r1s0, play_r1s1, play_r1s2, play_r1s3, play_r1s4, play_r1s5, play_r1s6, play_r1s7, play_r1s8, play_r1s9],
